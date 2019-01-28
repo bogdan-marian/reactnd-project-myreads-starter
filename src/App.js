afbcc13d68cfc69804004be4a3e30f49c2ea06b3
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2019-01-28 22:41:07 +0100

    wraped button in link

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookSelf from './BookSelf'
+import { Link } from 'react-router-dom'
 
 
 
@@ -76,7 +77,11 @@ class BooksApp extends React.Component {
           </div>
         </div>
         <div className="open-search">
-          <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+          {/* <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button> */}
+          <Link
+            to='/search'>
+            <button >Add a book</button>
+          </Link>
         </div>
       </div>
     )
